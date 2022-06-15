fb86cf5988b69d298a1469f42fd65e8fa4969046
Edu-Lu <moralesluis17106494@gmail.com>
2022-06-15 00:22:15 -0500

    ejercicio terminado

--- src/main.kt
+++ src/main.kt
@@ -5,8 +5,10 @@
  * Fecha publicación resolución: 10/01/22
  * Dificultad: MEDIA
  *
- * Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
- * Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+ * Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero
+ * o falso (Boolean) según sean o no anagramas.
+ * Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra
+ * palabra inicial.
  * NO hace falta comprobar que ambas palabras existan.
  * Dos palabras exactamente iguales no son anagrama.
  *
@@ -17,6 +19,35 @@
  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
  *
  */
-fun main(){
+fun main() {
+    println("Ingresa la primer palabra")
+    val palabra1 = readLine()
+    println("Ingresa la segunda palabra")
+    val palabra2 = readLine()
+
+    if (palabra1!!.length == palabra2!!.length)
+        anagrama(palabra1, palabra2)
+    else
+        println("No es un anagrama")
+
+
+}
+
+fun anagrama(palabra1: String, palabra2: String) {
+    val arrayPalabra1 = arrayListOf<Char>()
+    val arrayPalabra2 = arrayListOf<Char>()
+    palabra1.lowercase().forEach {
+        arrayPalabra1.add(it)
+    }
+    palabra2.lowercase().forEach {
+        arrayPalabra2.add(it)
+    }
+    if (arrayPalabra1.sorted() == arrayPalabra2.sorted()) {
+        println("$palabra1 y $palabra2 forman un anagrama")
+
+    } else {
+        println("No es un anagrama")
+
+    }
 
 }
